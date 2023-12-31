6a28b496d9d0398cf2feb32fbd26ee2e4f3c4286
Hallam Smith <me@hcssmith.com>
2023-12-31 10:36:24 +0000

    Enable custom keybinds in setup function

--- lua/hcssmith-core-keybinds/fn.lua
+++ lua/hcssmith-core-keybinds/fn.lua
@@ -1,15 +1,20 @@
 local fn = {}
 
+
+---@param keymap_tbl KeyBindList
+---@param bufnr number?
 function fn.SetKeymaps(keymap_tbl, bufnr)
   local modes = {
-    { "insert","i"},
-    { "normal","n"},
-    { "visual","v"},
-    { "terminal","t"}
+    { "insert",   "i" },
+    { "normal",   "n" },
+    { "visual",   "v" },
+    { "terminal", "t" }
   }
   for _, v in ipairs(modes) do
     if keymap_tbl[v[1]] ~= nil then
       for _, km in ipairs(keymap_tbl[v[1]]) do
+        ---@type KeyBind
+        km = km
         local opts = km[3]
         if opts == nil and bufnr ~= nil then
           opts = {
@@ -21,6 +26,5 @@ function fn.SetKeymaps(keymap_tbl, bufnr)
     end
   end
 end
-
 
 return fn
--- lua/hcssmith-core-keybinds/init.lua
+++ lua/hcssmith-core-keybinds/init.lua
@@ -1,32 +1,49 @@
 local M = {}
 
+---@type KeyBindList
 local keymap = {
-    insert = {
-      {"jk", "<Esc>"}
-    },
-    terminal = {
-      {"jk", "<C-\\><C-N>"}
-    },
-    normal = {
-      {"<C-h>", "<C-W><C-h>"},
-      {"<C-j>", "<C-W><C-j>"},
-      {"<C-k>", "<C-W><C-k>"},
-      {"<C-l>", "<C-W><C-l>"},
-      {"<C-Left>", "<C-W><C-h>"},
-      {"<C-Down>", "<C-W><C-j>"},
-      {"<C-Up>", "<C-W><C-k>"},
-      {"<C-Right>", "<C-W><C-l>"},
-    }
+  insert = {
+    { "jk", "<Esc>" }
+  },
+  terminal = {
+    { "jk", "<C-\\><C-N>" },
+  },
+  normal = {
+    { "<C-h>",     "<C-W><C-h>" },
+    { "<C-j>",     "<C-W><C-j>" },
+    { "<C-k>",     "<C-W><C-k>" },
+    { "<C-l>",     "<C-W><C-l>" },
+    { "<C-Left>",  "<C-W><C-h>" },
+    { "<C-Down>",  "<C-W><C-j>" },
+    { "<C-Up>",    "<C-W><C-k>" },
+    { "<C-Right>", "<C-W><C-l>" },
+  }
 }
 
+---@class KeyBind
+---@field [1] string keybind trigger
+---@field [2] string | function() keybind actions
+---@field [3] table? opts
 
+---@class KeyBindList
+---@field insert KeyBind[] | nil
+---@field terminal KeyBind[] | nil
+---@field normal KeyBind[] | nil
+---@field visual KeyBind[] | nil
+
+---@class Opts
+---@field keybinds KeyBindList?
+
+---@param opts Opts
 function M.setup(opts)
-   opts = opts or {}
+  opts = opts or {}
 
-   local fn = require('hcssmith-core-keybinds.fn')
+  local fn = require('hcssmith-core-keybinds.fn')
 
-   fn.SetKeymaps(keymap)
-
+  fn.SetKeymaps(keymap)
+  if opts.keybinds ~= nil then
+    fn.SetKeymaps(opts.keybinds)
+  end
 end
 
 return M
